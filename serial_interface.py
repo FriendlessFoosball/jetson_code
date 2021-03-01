941d641bcd31861c86a67901361fc77dc13e0549
Nick Alvarez <nalvarez@andrew.cmu.edu>
2021-02-28 20:11:44 -0500

    Added Serial Interface

--- serial_interface.py
+++ serial_interface.py
@@ -1,18 +1,61 @@
 import zmq
 import multiprocessing as mp
 import time
+import serial
 
+def convertToBytes(value):
+    return value
 
 def serial_recv(endpoint):
+    ser = serial.Serial()
+    ser.timeout = 1
+    ser.baudrate = 1843200
+    ser.port = '/dev/tty.usbmodem14201'
+    ser.open()
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.setsockopt(zmq.RCVHWM, 10)
     socket.bind('tcp://localhost:5558')
     socket.subscribe('')
 
+    command = {
+        "move_zero": 0,
+        "set_zero": 1,
+        "set_speed": 2,
+        "set_position": 3,
+        "get_position": 4,
+        "set_all_positions": 5,
+        "set_all_speeds": 6,
+        "get_all_speeds": 7
+    }
+    motors = ["x_val", "y_val", "z_val", "a_val"]
     while True:
+        """
+            Item is structured like dictionary:
+                KEY         VALUE
+                commmand    "move_zero", "set_zero", "set_speed", "set_position", "get_position", "set_all_positions", "set_all_speeds", "get_all_speeds"
+                x_val       value
+                y-val       value
+                z_val       value
+                a_val       value
+            Each motor 
+        """
         item = socket.recv_pyobj()
-        
+        comm_num = item["command"]
+        if comm_num < 5:
+            val = 0
+            motor = 0
+            for i in range(len(motors)):
+                if motors[i] in item:
+                    val = item[motors[i]]
+                    motor = i
+            first_byte = comm_num * 4 + motor
+            first_byte = first_byte.to_bytes(1, "big")
+            arr = [first_bytes]
+            arr.extend(convertToBytes(val))
+            arr.append(0x0A)
+            ser.write(bytearray(arr))
+
 
 
 class SerialInterface:
